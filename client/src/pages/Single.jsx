c34fda0f31ccf3780628b749355b9f74e7d63613
DominikaDuralek <dominikaduralek@gmail.com>
2024-06-02 13:58:35 +0200

    Added more UI components

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,3 +1,7 @@
+import { Link } from 'react-router-dom';
+import Edit from '../img/edit.png';
+import Delete from '../img/delete.png';
+
 function Single() {
     return (
       <div className="single">
@@ -9,9 +13,17 @@ function Single() {
               <span>John</span>
               <p>Posted 2 days ago</p>
             </div>
+            <div className="edit">
+              <Link to={'/write?edit=2'}>
+                <img src={Edit} alt="edit" />
+              </Link>
+              <img src={Delete} alt="delete" />
+            </div>
           </div>
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis dolores dicta quas ad, incidunt accusantium sequi ea sed consectetur, beatae suscipit.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis culpa nisi odit vel ut voluptatibus, debitis quas officia modi, totam earum nemo ipsam qui iste eligendi minima? Minima, enim praesentium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis culpa nisi odit vel ut voluptatibus, debitis quas officia modi, totam earum nemo ipsam qui iste eligendi minima? Minima, enim praesentium? Minima, enim praesentium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis culpa nisi odit vel ut voluptatibus, debitis quas officia modi, totam earum nemo ipsam qui iste eligendi minima? Minima, enim praesentium?</p>
         </div>
-        <div className="menu">m</div>
+        <Menu />
       </div>
     );
   }
