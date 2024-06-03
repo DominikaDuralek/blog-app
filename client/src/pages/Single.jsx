b2133763e16847aef624d7c662cbb48c682c7762
DominikaDuralek <dominikaduralek@gmail.com>
2024-06-03 16:40:52 +0200

    Added react-quill

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import Menu from "../components/Menu";
 import Edit from '../img/edit.png';
 import Delete from '../img/delete.png';
 
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 function Write() {
-    return (
-      <div>
-        Write!
+  const [value, setValue] = useState('');
+
+  console.log(value);
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill theme="snow" value={value} onChange={setValue} />
+        </div>
       </div>
-    );
-  }
-  
-  export default Write;
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+        </div>
+        <div className="item">item 2</div>
+      </div>
+    </div>
+  );
+}
+
+export default Write;
